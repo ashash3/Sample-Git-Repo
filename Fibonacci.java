067148736a9dcec84fe14bee7cc3c84e228430d7
Ashley Chou <ashchou3@gmail.com>
2013-02-04 22:18:58 -0500

    added fib file

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Fibonacci {
+	
+	public static void main(String[] args) {
+		Fibonacci f = new Fibonacci();
+		int result = f.calculate(Integer.parseInt(args[0]));
+		System.out.println(result);
+	}
+
+	// Returns the nth fibonnaci number, assuming 0 is the 0th 
+	// fibonnaci number in the sequence
+	private int calculate(int nthNum) {
+		return 0;
+	}
+}
