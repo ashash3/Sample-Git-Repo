fff487bd13fd824aba7c2751e561a73a09d705d3
Ashley Chou <ashchou3@gmail.com>
2013-02-04 22:25:32 -0500

    Adding DP implementation

--- Fibonacci.java
+++ Fibonacci.java
@@ -11,6 +11,12 @@ public class Fibonacci {
 	// Returns the nth fibonnaci number, assuming 0 is the 0th 
 	// fibonnaci number in the sequence
 	private int calculate(int nthNum) {
-		return 0;
+		int[] dp = new int[nthNum+1];
+		dp[0] = 0;
+		dp[1] = 1;
+		for (int i = 2; i <= nthNum; i++) {
+			dp[i] = dp[i-1] + dp[i-2];
+		}
+		return dp[nthNum];
 	}
 }
